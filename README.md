4cda08e08d36ab1e3f10b659fa039a0cb839523a
JohnParkerHodges <phodgesfl@gmail.com>
2019-10-25 11:21:30 -0400

    added images path

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Hw04Real
-This is a Basic JavaScript quiz!
+# Hw04Real I wasn't ready for a quiz!!
+This is a Basic JavaScript quiz! 
+this 
 
-![image description]("assets/images/Screen Shot 2019-10-24 at 7.39.57 PM.png")
-![image description]("assets/images/Screen Shot 2019-10-24 at 7.40.21 PM.png")
+![image description](Hw04Real/assets/images/img1.png)
+![image description](Hw04Real/assets/images/img2.png)
 ![image description]("assets/images/Screen Shot 2019-10-24 at 7.40.36 PM.png")
 ![image description]("assets/images/Screen Shot 2019-10-24 at 7.40.42 PM.png")
-![image description]("assets/images/Screen Shot 2019-10-24 at 7.40.57 PM.png")
+![image description]("assets/images/Screen Shot 2019-10-") 
---
+++ game.js
@@ -0,0 +1,211 @@
+const startButton = document.getElementById('startQuiz');
+const greeting = document.getElementById('greeting');
+const questionDiv = document.getElementById('question');
+const timerDiv = document.getElementById('timer');
+const scoreDiv = document.getElementById('score');
+const finalScoreDiv = document.getElementById('finalScore');
+const currentResultDiv = document.getElementById('currentResult');
+const wholeGameDiv = document.getElementById('wholeGame');
+const endResultsDiv = document.getElementById('endResults');
+const intialsInput = document.getElementById('intialsInput');
+const highScoreContentDiv = document.getElementById('highScoreContent');
+const highScoreList = document.getElementById('highScoreList');
+const choiceA = document.getElementById('choiceA');
+const choiceB = document.getElementById('choiceB');
+const choiceC = document.getElementById('choiceC');
+const choiceD = document.getElementById('choiceD');
+
+let currentQuestionNumber = 0;
+let score = 0;
+let timer = 100;
+let timerInterval;
+
+startButton.addEventListener('click', start);
+
+function start() {
+    startButton.classList.add('d-none');
+    greeting.classList.add('d-none');
+    wholeGameDiv.classList.remove('d-none');
+    timerInterval = setInterval(() => {
+        timerDiv.innerHTML = timer;
+        timer--;
+        timer <= 0 && endGame();
+    }, 1000);
+    renderQuestion();
+}
+
+function renderQuestion() {
+    if (currentQuestionNumber > 9) {
+        endGame();
+    } else {
+        const currentQuestion = questions[currentQuestionNumber];
+        questionDiv.innerHTML = currentQuestion.question;
+        choiceA.innerHTML = currentQuestion.choiceA;
+        choiceB.innerHTML = currentQuestion.choiceB;
+        choiceC.innerHTML = currentQuestion.choiceC;
+        choiceD.innerHTML = currentQuestion.choiceD;
+    }
+}
+
+function checkAnswer(choice) {
+    choice === questions[currentQuestionNumber].correct ? correct() : incorrect();
+    currentQuestionNumber++;
+    renderQuestion();
+}
+
+function correct() {
+    score++;
+    scoreDiv.innerHTML = score;
+    currentResultDiv.innerHTML = 'Correct!';
+    setTimeout(() => {
+        currentResultDiv.innerHTML = '';
+    }, 2000);
+}
+
+function incorrect() {
+    timer -= 3;
+    timerDiv.innerHTML = timer;
+    currentResultDiv.innerHTML = 'Incorrect!';
+    setTimeout(() => {
+        currentResultDiv.innerHTML = '';
+    }, 2000);
+}
+
+function endGame() {
+    clearInterval(timerInterval);
+    wholeGameDiv.classList.add('d-none');
+    finalScoreDiv.innerHTML = score;
+    endResultsDiv.classList.remove('d-none');
+}
+
+function handleHighScoreSubmit(e) {
+    e.preventDefault();
+    localStorage.setItem(intialsInput.value, score);
+    endResultsDiv.classList.add('d-none');
+    highScoreContentDiv.classList.remove('d-none');
+    renderHighScores();
+}
+
+function renderHighScores() {
+    let sortedLocalObjects = getSortedLocalStorageScores();
+
+    for (let i = 0; i < sortedLocalObjects.length; i++) {
+        let li = document.createElement('li');
+        let text = document.createTextNode(`name: ${sortedLocalObjects[i].name} score: ${sortedLocalObjects[i].score}`);
+        li.appendChild(text);
+        highScoreList.appendChild(li);
+    }
+}
+
+function getSortedLocalStorageScores() {
+    // create empty array to hold my objects
+    let localStorageObjects = [];
+    // loop over local storage
+    for (let i = 0; i < localStorage.length; i++) {
+        //check if the value of the local storage key can be parsed into a number
+        Number(localStorage.getItem(localStorage.key(i))) &&
+            // if yes, make an object and put it into my array
+            localStorageObjects.push({
+                // key will be the name
+                name: localStorage.key(i),
+                // getItem will be the score
+                score: localStorage.getItem(localStorage.key(i))
+            })
+    }
+    // give back a sorted version (descending) of the array I built
+    return localStorageObjects.sort((a, b) => {
+        return b.score - a.score
+    })
+}
+
+let questions = [{
+        question: 'Inside which HTML element do we put the JavaScript?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: '&lt;script&gt;',
+        choiceB: '&ltjavascript&gt',
+        choiceC: '&ltjs&gt',
+        choiceD: '&ltscripting&gt',
+        correct: 'A',
+    },
+    {
+        question: 'How to write an IF statement in JavaScript?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'if (i == 5)',
+        choiceB: 'if i = 5 then',
+        choiceC: 'if i = 5',
+        choiceD: 'if i == 5 then',
+        correct: 'A',
+    },
+    {
+        question: 'Where is the correct place to insert a JavaScript?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'The &ltbody&gt section',
+        choiceB: 'Both the &lthead&gt section and the &ltbody&gt section are correct  ',
+        choiceC: 'The &lthead&gt section',
+        choiceD: 'The &ltHeader&gt section',
+        correct: 'B',
+    },
+    {
+        question: 'The external JavaScript file must contain the &ltscript&gt tag.',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'False',
+        choiceB: 'True',
+        choiceC: '&ltbody&gt',
+        choiceD: '&ltlink&gt',
+        correct: 'A',
+    },
+    {
+        question: 'How does a WHILE loop start?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'while (i <= 10)',
+        choiceB: 'while i = 1 to 10',
+        choiceC: 'while (i <= 10; i++)',
+        choiceD: 'For ([i] =< 10; i+-',
+        correct: 'A',
+    },
+    {
+        question: 'How does a FOR loop start?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'for (i = 0; i <= 5; i++)',
+        choiceB: 'for i = 1 to 5',
+        choiceC: 'for (i = 0; i <= 5)',
+        choiceD: 'for (i <= 5; i++)',
+        correct: 'A',
+    },
+    {
+        question: 'How do you round the number 7.25, to the nearest integer?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'Math.round(7.25)',
+        choiceB: 'Math.rnd(7.25)',
+        choiceC: 'rnd(7.25)',
+        choiceD: 'round(7.25)',
+        correct: 'A',
+    },
+    {
+        question: 'How do you find the number with the highest value of x and y?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'Math.max(x, y)',
+        choiceB: 'ceil(x, y)',
+        choiceC: 'Math.ceil(x, y)',
+        choiceD: 'top(x, y)',
+        correct: 'A',
+    },
+    {
+        question: 'JavaScript is the same as Java.',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'False',
+        choiceB: 'True',
+        choiceC: 'False its jQuery',
+        choiceD: 'maybe',
+        correct: 'A',
+    },
+    {
+        question: 'Which event occurs when the user clicks on an HTML element?',
+        imgSrc: './assets/images/1_HP8l7LMMt7Sh5UoO1T-yLQ.png',
+        choiceA: 'onclick  ',
+        choiceB: 'onmouseclick',
+        choiceC: 'onchange',
+        choiceD: 'onmouseover',
+        correct: 'A',
+    },
+];
